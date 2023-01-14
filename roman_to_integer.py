0c72b193d341d2b42f14cd6ddf0e40aae70e823a
elnaz <elnaz.nasiri92@gmail.com>
2023-01-14 10:35:31 +0330

    Add Python challenges 13-Roman to Integer

---
+++ roman_to_integer.py
@@ -0,0 +1,23 @@
+def romanToInt(s: str)->int:
+    lookup = {
+        "I":1
+        ,"V":5
+        ,"X":10
+        ,"L":50
+        ,"C":100
+        ,"D":500
+        ,"M":1000
+    }
+    N = len(s)
+    i = N - 1
+    output = 0
+    while i>=0:
+        if i < N-1 and lookup[s[i]] < lookup[s[i+1]]:
+            output -= lookup[s[i]]
+        else:
+            output += lookup[s[i]]
+        i -= 1
+    return output
+
+
+print(romanToInt("IV"))
