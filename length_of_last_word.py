8ebf54f9248ccb984c0534edc2f90b5d9a8967d9
elnaz <elnaz.nasiri92@gmail.com>
2023-01-16 22:07:00 +0330

    Add New Solution

--- length_of_last_word.py
+++ length_of_last_word.py
@@ -1,6 +1,15 @@
+#solution 1
 def lenghtOfLastWord(s):
     verb = s.split()
     return len(verb[-1])
 
 print(lenghtOfLastWord("luffy is still joyboy"))
 
+
+#solution 2
+def lenghtOfLastWord(s):
+    strip = s.strip()
+    word = strip.split()
+    return len(word[-1])
+    
+print(lenghtOfLastWord("luffy is still joyboy"))
