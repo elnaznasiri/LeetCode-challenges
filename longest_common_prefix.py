d5fe4e28aac0ca66f20d3a3651ed23143bc49564
elnaz <elnaz.nasiri92@gmail.com>
2023-01-20 10:48:26 +0330

    Add Base Condition

--- longest_common_prefix.py
+++ longest_common_prefix.py
@@ -2,6 +2,13 @@ def longestCommonPrefix(strs):
 
     result = ""
 
+    if strs is None or len(strs) == 0:
+        return result
+        
+    # If there is only one element in array then return it
+    if len(strs) == 1:
+        return strs[0]
+
     # Find the minimum length string from the array
     minimum_length = len(strs[0])
     for i in range(1, len(strs)):
@@ -12,7 +19,7 @@ def longestCommonPrefix(strs):
 
         # Get the current character from the first string
         current = strs[0][i]
-        
+
         # Check if this character is found in all other strings or not
         for j in range(0, len(strs)):
             if strs[j][i] != current:
