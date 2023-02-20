c536bbb3ef69250073d84f9f291fa7ea311002a2
elnaz <elnaz.nasiri92@gmail.com>
2023-02-20 20:32:21 +0330

    Add Python challenges 507-Perfect Number

---
+++ perfect_number.py
@@ -0,0 +1,13 @@
+def checkPerfectNumber(num):
+  """
+  :type num: int
+  :rtype: bool
+  """
+  ans = 1
+  div = 2
+  while div ** 2 <= num:
+    if num % div == 0:
+      ans += div
+      ans += num / div
+    div += 1
+  return ans == num if num != 1 else False
