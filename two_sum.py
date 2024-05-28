a6088ee7fa3490e53d0c394d7479c1931dc41667
elnaznasiri <elnaz.nasiri92@gmail.com>
2024-05-28 22:18:09 +0330

    Write New Solution

--- two_sum.py
+++ two_sum.py
@@ -1,5 +1,7 @@
-
+# solution1
 def twoSum(nums, target):
+    # key->[7,2,3]
+    # value->0,1,2
     values = {}
     for idx, value in enumerate(nums):
         if target - value in values:
@@ -7,6 +9,17 @@ def twoSum(nums, target):
         else:
             values[value] = idx
 
+nums = [7,2,3]
+target=9
+print(twoSum(nums, target))
 
+
+
+
+# solution2
+def twoSum(nums, target):
+    for i in range(len(nums)):
+        for j in range(i + 1,len(nums)):
+            if target == nums[i]+nums[j]:
+                return [i,j]
 print(twoSum([2,11,7,15], 9))
-
